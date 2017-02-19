eb83bdc4bf7c2d72f2d3aaa4cb53a2d43da2bfaf
marco gallegos <ma_galeza@hotmail.com>
2017-02-19 07:19:04 -0600

    definiciones y mantenimiento  Changes to be committed: 	modified:   manjaro.sh

--- manjaro.sh
+++ manjaro.sh
@@ -4,19 +4,44 @@ val_apm=$(atom --version)
 if [[ ! $val_apm ]]; then
   val_apm="---- No tienes instalado atom -----"
 fi
+opcion="basura :v"
 
-opcion=$(zenity --list\
- --title="Algunas opciones comunes para despues de instalar Manjaro XFCE"\
- --radiolist\
- --width="700"\
- --height="500"\
- --column="" --column="Opcion" --column="Descripcion" --column="Estado actual"\
- FALSE  "Actualizar"        "Actualizar el sistema"                     "-" \
- FALSE  "Limpiar"           "limpar la cache de pacman"                 "-" \
- FALSE  "Software"          "software basico "                          "-"\
- FALSE  "IDES"              "IDE's y editores que uso para programar"   "-"\
- FALSE  "Swappiness"        "editar el uso de la swap"                  "$val_swappines"\
- FALSE  "Complementos ATOM" "complementos basicos para el editor atom"  "$val_apm"
-)
+while [[ $opcion != "" ]]; do
+  opcion=$(zenity --list\
+   --title="Algunas opciones comunes para despues de instalar Manjaro XFCE"\
+   --radiolist\
+   --width="700"\
+   --height="500"\
+   --column="" --column="Opcion" --column="Descripcion" --column="Estado actual"\
+   TRUE   "Actualizar"        "Actualizar el sistema"                     "-" \
+   FALSE  "Limpiar"           "limpar la cache de pacman"                 "-" \
+   FALSE  "Software"          "software basico "                          "-"\
+   FALSE  "IDES"              "IDE's y editores que uso para programar"   "-"\
+   FALSE  "Swappiness"        "editar el uso de la swap"                  "$val_swappines"\
+   FALSE  "Complementos ATOM" "complementos basicos para el editor atom"  "$val_apm"\
+   FALSE  "SUDO"              "eliminar el periodo de gracia de sudo"     "-"\
 
-echo $opcion
+  )
+
+  case $opcion in
+    "Actualizar" )
+    pamac-updater
+      ;;
+    "Limpiar" )
+    sudo pacman -Scc
+      ;;
+
+    "Software" )
+    sudo pacman -S bleachbit vlc-nightly
+      ;;
+
+    "IDES" )
+    sudo pacman -S qt5-tools qtcreator gdb gcc-multilib geany geany-plugins atom texlive-core texmaker
+      ;;
+
+    "SUDO" )
+    #Defaults	timestamp_timeout=0
+      ;;
+  esac
+
+done
