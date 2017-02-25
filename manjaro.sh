9e378f5db8f3ed18dcd04f26ea835fb6836dd34f
marco gallegos <ma_galeza@hotmail.com>
2017-02-25 02:01:14 -0600

    implementaciones  On branch dev  Changes to be committed: 	modified:   manjaro.sh

--- manjaro.sh
+++ manjaro.sh
@@ -13,13 +13,14 @@ while [[ $opcion != "" ]]; do
    --width="700"\
    --height="500"\
    --column="" --column="Opcion" --column="Descripcion" --column="Estado actual"\
-   TRUE   "Actualizar"        "Actualizar el sistema"                     "-" \
-   FALSE  "Limpiar"           "limpar la cache de pacman"                 "-" \
-   FALSE  "Software"          "software basico "                          "-"\
-   FALSE  "IDES"              "IDE's y editores que uso para programar"   "-"\
-   FALSE  "Swappiness"        "editar el uso de la swap"                  "$val_swappines"\
-   FALSE  "Complementos ATOM" "complementos basicos para el editor atom"  "$val_apm"\
-   FALSE  "SUDO"              "eliminar el periodo de gracia de sudo"     "-"\
+   TRUE   "Actualizar"        "Actualizar el sistema"                               "-" \
+   FALSE  "Limpiar"           "Limpar la cache de pacman"                           "-" \
+   FALSE  "Software"          "Software basico "                                    "-"\
+   FALSE  "IDES"              "IDE's y editores que uso para programar"             "-"\
+   FALSE  "Swappiness"        "Editar el uso de la swap"                            "$val_swappines"\
+   FALSE  "Complementos ATOM" "Complementos basicos para el editor atom"            "$val_apm"\
+   FALSE  "SUDO"              "Eliminar el periodo de gracia de sudo"               "-"\
+   FALSE  "Cargar SSH"        "Reutilizar tu clave ssh copiada en ~/.ssh"           "-"
 
   )
 
@@ -29,23 +30,28 @@ while [[ $opcion != "" ]]; do
       ;;
     "Limpiar" )
     sudo pacman -Scc
+    yaourt -Scc
       ;;
 
     "Software" )
-    sudo pacman -S bleachbit vlc-nightly
+    gksu pacman -S bleachbit vlc-nightly
       ;;
 
     "IDES" )
-    sudo pacman -S qt5-tools qtcreator gdb gcc-multilib geany geany-plugins atom texlive-core texmaker
+    gksu "pacman -S qt5-tools qtcreator gdb gcc-multilib geany geany-plugins atom texlive-core texmaker"
       ;;
     "Swappiness" )
-    echo vm.swappiness=40 >> /etc/sysctl.d/99-sysctl.conf
+    su -c "echo vm.swappiness=40 >> /etc/sysctl.d/99-sysctl.conf"
       ;;
-      "Complementos ATOM" )
-      sudo apm install color-picker emmet linter linter-cppcheck file-icons atom-ternjs atom-bootstrap3 pigments highlight-selected open-recent autocomplete-python platformio-ide-terminal
+    "Complementos ATOM" )
+    gksu "apm install color-picker emmet linter linter-cppcheck file-icons atom-ternjs atom-bootstrap3 pigments highlight-selected open-recent autocomplete-python platformio-ide-terminal atom-dark-fusion-syntax"
       ;;
     "SUDO" )
     #Defaults	timestamp_timeout=0
+    gksu "echo Defaults	timestamp_timeout=0.2 >> /etc/sudoers"
+      ;;
+    "Cargar SSH" )
+    ssh-add ~/.ssh/id_rsa
       ;;
   esac
 
