707617efef577231da7d466862c9619a91a5dfe8
Marco A. Gallegos <ma_galeza@hotmail.com>
2018-03-31 15:52:11 -0600

    bugfix : comando atom incompleto

--- manjaro.sh
+++ manjaro.sh
@@ -24,7 +24,7 @@ if [[ ! $val_atom ]]; then
   val_atom="Atom\nNo tienes instalado atom"
   aviso $val_atom
   aviso "Atom\nInstalando atom"
-  echo $sudo_pass | sudo
+  echo $sudo_pass | sudo -S pacman -S --noconfirmation atom
   aviso "Atom\nAhora esta instalado en tu sistema"
 fi
 
