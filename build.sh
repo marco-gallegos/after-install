f9d50cdbce5c3170b9551e31cfbd5081130fb431
marco-gallegos <ma_galeza@hotmail.com>
2020-01-08 00:43:04 -0600

    [imp] docker,fedora: se actualiza
    se actualiza el soporte para docker con el script, se usara un script
    solo de consolo ahora

--- build.sh
+++ build.sh
@@ -5,11 +5,19 @@
 * @descripcion 
 '
 
-docker-compose up --build -d
+docker rm fedorafter
+docker rmi fedorafter:0.1
 
-#docker build . -t fedorafter:0.1
-#docker run --name fedorafter -it fedorafter:0.1
+#docker-compose up --build
+
+docker build . -t fedorafter:0.1
+docker run --name fedorafter -it fedorafter:0.1
 
 # salir ctrl + p +q
+# para ingresar a contenedor corriendo
 # docker attach "nombre"
-docker attach fedorafter
+# docker attach fedorafter
+
+# para ingresar a contenedor apagado
+# docker start -i "nombre"
+#docker start -i fedorafter
