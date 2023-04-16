ec74b402d926c0aac59cb6939f97616dfe7a161d
marco.gallegos <marco.gallegos@whym.tech>
2023-04-16 00:35:08 -0600

    chore(nvim): missing config on lsp

--- nvim/lazy/.config/nvim/lua/plugins/lsp.lua
+++ nvim/lazy/.config/nvim/lua/plugins/lsp.lua
@@ -56,8 +56,8 @@ return {
             --['<C-Space>'] = cmp.mapping.complete(),
 
             -- Navigate between snippet placeholder
-            --['<C-f>'] = cmp_action.luasnip_jump_forward(),
-            --['<C-b>'] = cmp_action.luasnip_jump_backward(),
+            ['<C-f>'] = cmp_action.luasnip_jump_forward(),
+            ['<C-b>'] = cmp_action.luasnip_jump_backward(),
           }
         })
 	end,
